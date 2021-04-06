aa7c859685fb3403a8c2ddeef3f82f0126cb6627
unknown <sungwano@gmail.com>
2021-04-06 16:19:41 +0700

    make it better

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from linebot.models import *
 from linebot import *
 import json
 import requests
-from mysql.connector import connection
 
 app = Flask(__name__)
 
@@ -60,112 +59,63 @@ def reply(intent,text,reply_token,id,disname):
     elif intent == 'company_addr':
         compgrp_id = ''
         text = text.upper()
-        if text.find("TIC") >= 0 :
-            compgrp_id = 'TIC'
-        elif text.find("STC") >= 0 :
-            compgrp_id = 'STC'
-        elif text.find("TNH") >= 0 :
-            compgrp_id = 'TNH'
-        add = getAdd(compgrp_id)
-        text_message = TextSendMessage(text=add)
+        if text.find("TIC") >= 0 or text.find("STC") >= 0 or text.find("TNH") >= 0 or text.find("PYA") >= 0 or text.find("DTC") >= 0 or text.find("DRD") >= 0:
+            compgrp_id = text
+
+        path = "address/"
+        file1 = open(path + compgrp_id+".txt", encoding="utf8")
+        addr = ""
+        while True:
+            # Get next line from file
+            line = file1.readline()
+            if not line:
+                break
+
+            addr += line.strip()+"\n"
+
+        file1.close()
+        #add = getAdd(compgrp_id)
+        text_message = TextSendMessage(text=addr)
+
     elif intent == 'ext_number':
         dep_id = ''
         text = text.upper()
         if text.find("ไอที") >= 0 or text.find("IT") >= 0 :
-            dep_id = 'ไอที'
+            dep_id = 'IT'
         elif text.find("ตปท") >= 0 or text.find("ต่างประเทศ") >= 0 or text.find("FOREIGN") >= 0:
-            dep_id = 'ต่างประเทศ'
+            dep_id = 'FOREIGN'
         elif text.find("บัญชี") >= 0 or text.find("ACCOUNT") >= 0 :
-            dep_id = 'บัญชี'
+            dep_id = 'ACCOUNT'
         elif text.find("สินค้า") >= 0 or text.find("สินค้าขาย") >= 0 or text.find("PRODUCT") >= 0 :
-            dep_id = 'สินค้าขาย'
+            dep_id = 'PRODUCT'
         elif text.find("ขาย") >= 0 or text.find("ตลาด") >= 0 or text.find("การตลาด") >= 0 or text.find("SALES") >= 0 :
-            dep_id = 'การตลาด'
+            dep_id = 'SALES'
         elif text.find("การเงิน") >= 0 or text.find("FINANCE") >= 0 :
-            dep_id = 'การเงิน'
+            dep_id = 'FINANCE'
         elif text.find("แลป") >= 0 or text.find("เทคนิค") >= 0 or text.find("LAB") >= 0:
             dep_id = 'LAB'
         elif text.find("ผลิต") >= 0 or text.find("PRODUCTION") >= 0:
-            dep_id = 'ผลิต'
+            dep_id = 'PRODUCTION'
         elif text.find("คลัง") >= 0 or text.find("คลังสินค้า") >= 0 or text.find("GODOWN") >= 0:
-            dep_id = 'คลังสินค้า'
+            dep_id = 'GODOWN'
         elif text.find("จัดส่ง") >= 0 or text.find("LOGISTIC") >= 0:
-            dep_id = 'จัดส่ง'
+            dep_id = 'LOGISTIC'
         elif text.find("HR") >= 0 or text.find("ธุรการ") >= 0 or text.find("บุคคล") >= 0:
-            dep_id = 'ธุรการและการบุคคล'
-        extNumber = getExtNumber(dep_id)
+            dep_id = 'HR'
+        #extNumber = getExtNumber(dep_id)
+
+        path = "ext_number/"
+        file1 = open(path + dep_id+".txt", encoding="utf8")
+        file_as_list = file1.readlines()
+        extNumber = ""
+        for line in file_as_list:
+            # Get next line from file
+            #line = file1.readline()
+            extNumber += line.strip()+"\n"
+        file1.close()
         text_message = TextSendMessage(text=extNumber)
 
-
     line_bot_api.reply_message(reply_token, text_message)
-
-def getAdd(compgrpID):
-
-    username = "vpcbot"
-    passwd = "vpcbotvpcbot"
-    host_name = "vpc-group.dyndns-office.com"
-    db_name = "vpcbot"
-    conn = connection.MySQLConnection(user=username, password=passwd,
-                                          host=host_name, database=db_name)
-    cursor = conn.cursor()
-
-    #cursor.execute("SELECT compgrp_code,compgrp_id,compgrp_tname,address1,address2,CONCAT(district,' ',province,' ',postal_code), tax_id FROM vpcbot.company_add WHERE LENGTH(compgrp_id)>0 AND compgrp_id='" + compgrpID + "'  ORDER BY compgrp_code")
-    cursor.execute("SELECT compgrp_code,compgrp_id,compgrp_tname,address1,address2,CONCAT(district,' ',province,' ',postal_code), tax_id FROM vpcbot.company_add WHERE LENGTH(compgrp_id)>0 AND compgrp_id='" + compgrpID + "'  ORDER BY compgrp_code")
-    addr = ""
-    for r in cursor.fetchall():
-        addr = (r[2]).strip() + "\n" + r[3].strip() + "\n"
-        if len(str(r[4]).strip()) > 0:
-            addr = addr + r[4].strip() + "\n"
-        addr = addr + r[5] + "\n\n"
-        addr = addr + "เลขที่ผู้เสียภาษี " + r[6]
-    addr = addr if len(addr) > 0 else 'ไม่พบข้อมูลที่อยู่บริษัทครับ!!! รบกวนให้ข้อมูลผมใหม่นะครับ'
-    return addr
-
-def getExtNumber(dep_id):
-
-    username = "vpcbot"
-    passwd = "vpcbotvpcbot"
-    host_name = "vpc-group.dyndns-office.com"
-    db_name = "vpcbot"
-    info_emoji = u'\U00002139'
-    telephone_emoji = u'\U0000260E'
-
-
-    conn = connection.MySQLConnection(user=username, password=passwd,
-                                          host=host_name, database=db_name)
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT dep_name, first_name, ext_number, email FROM vpcbot.users WHERE LENGTH(ext_number)>0 AND dep_name LIKE %s  ORDER BY dep_name, ext_number", ('%' + dep_id + '%',))
-    ext_number = ""
-    isFirst = 1
-    ifLab = 1 if (dep_id.find("LAB")>=0) else 0
-    oDepName = ""
-    for r in cursor.fetchall():
-        #print(r)
-        if isFirst :
-           if ifLab:
-              ext_number = info_emoji + " เบอร์ติดต่อภายในของ LAB\n"
-           else:
-               ext_number = info_emoji + " เบอร์ติดต่อภายในของ " + r[0] + "\n"
-        if ifLab:
-            if isFirst == 0 and r[0].strip() != oDepName:
-                ext_number +="\n"
-                ext_number +=  r[1].strip()+ " " + r[0] +" " + telephone_emoji +" "+ r[2].strip()
-            else:
-                ext_number +=  r[1].strip()+ " " + r[0] +" " + telephone_emoji +" "+ r[2].strip()
-            oDepName = r[0].strip()
-        else:
-            ext_number +=  r[1].strip()+ " " + telephone_emoji +" "+ r[2].strip()
-        isFirst = 0
-
-        '''
-        if len(r[3].strip()) > 0:
-           ext_number += " Email  "+r[3]
-        '''
-        ext_number +=  "\n"
-    ext_number = ext_number if len(ext_number) > 0 else 'ไม่มีแผนก '+dep_id+' ครับ !!! '
-
-    return ext_number
 
 if __name__ == "__main__":
     app.run()
